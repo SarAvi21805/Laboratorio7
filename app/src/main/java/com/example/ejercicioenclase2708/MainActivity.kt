c6f8c3d170e99a88d5061dc0b3e49b3460d8f171
SarAvi21805 <alejandraviles2005@gmail.com>
2025-09-10 18:54:16 -0600

    Importación de las configuraciones iniciales que indicó la profesora durante las clases.

---
+++ app/src/main/java/com/example/ejercicioenclase2708/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.ejercicioenclase2708
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ejercicioenclase2708.ui.theme.Ejercicioenclase2708Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Ejercicioenclase2708Theme {
+                PexelsScreen() // <- Solo agregar esto en el set Content en su theme
+                //ComposeUiOnlyApp()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Ejercicioenclase2708Theme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/ejercicioenclase2708/PexelsApi.kt
@@ -0,0 +1,14 @@
+package com.example.ejercicioenclase2708
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+interface PexelsApi {
+    @GET("v1/curated")
+    fun getCurated(
+        @Query("page") page: Int = 1,
+        @Query("per_page") perPage: Int = 20
+    ): Call<PexelsResponse>
+}
